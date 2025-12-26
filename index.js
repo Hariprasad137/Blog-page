4db5c2967f272279814f11de7161b2b1728a27d9
SilverCloud <obj268version4@gmail.com>
2025-12-26 21:24:27 +0530

    Vercel Friendly

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import bodyParser from "body-parser";
 import express from "express";
+import path from "path"; // 1. Import 'path' module
+
 const app = express();
 const port = 3000;
 var posts = [];
+
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs", { posts: posts });
+  res.render("index.ejs", { posts: posts });
 });
+
 app.get("/create", (req, res) => {
   res.render("createPosts.ejs");
 });
+
 app.post("/submit", (req, res) => {
   const post = {
     title: req.body["title"],
@@ -20,11 +29,17 @@ app.post("/submit", (req, res) => {
   posts.push(post);
   res.redirect("/");
 });
+
 app.post("/delete", (req, res) => {
     const index = req.body["postId"];
     posts.splice(index, 1);
     res.redirect("/");
-})
-app.listen(port, () => {
-  console.log(`Server Running on ${port}`);
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server Running on ${port}`);
+    });
+}
+
+export default app;
